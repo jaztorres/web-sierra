f5ca378828161cade324d220c38b22a394b7ed24
jaztorres <jazmin.torres@myma.mx>
2025-07-17 13:32:44 -0600

    update

---
+++ src/app/pages/cuenta/cuenta.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cuenta',
+  standalone: true,
+  imports: [],
+  templateUrl: './cuenta.component.html',
+  
+})
+export class CuentaComponent {
+
+}
---
+++ src/app/pages/registro-gracias/registro-gracias.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-registro-gracias',
+  standalone: true,
+  imports: [],
+  templateUrl: './registro-gracias.component.html',
+  styleUrl: './registro-gracias.component.css'
+})
+export class RegistroGraciasComponent {
+
+}
